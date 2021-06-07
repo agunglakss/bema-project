f5354671ab81c725b8f37f6886cce162af7a39a3
Agung.laksono <agung.laksono@aux.go-jek.com>
2021-06-07 19:32:25 +0700

    change link asset from asset to secure_asset

--- resources/views/layouts/user/source-footer.blade.php
+++ resources/views/layouts/user/source-footer.blade.php
@@ -11,4 +11,4 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 <!-- Lazy load -->
-<script src="{{ asset('assets/js/lazysizes.min.js') }}" async=""></script>
+<script src="{{ secure_asset('assets/js/lazysizes.min.js') }}" async=""></script>
--- resources/views/layouts/user/source-header.blade.php
+++ resources/views/layouts/user/source-header.blade.php
@@ -1,5 +1,5 @@
 <!-- Font Awesome -->
-<link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}" />
+<link rel="stylesheet" href="{{ secure_asset('assets/css/font-awesome.min.css') }}" />
 
 <!-- Bootstrap -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -11,5 +11,5 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">  
 
 <!-- custom css -->
-<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+<link rel="stylesheet" href="{{ secure_asset('assets/css/main.css') }}">
 <!-- end custom css-->
