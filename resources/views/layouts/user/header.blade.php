324273b7add0fba88c797f9f77a3cbc4e76767fb
Agung.laksono <agung.laksono@aux.go-jek.com>
2021-06-07 19:34:12 +0700

    change secure_asset in logo

--- resources/views/layouts/user/header.blade.php
+++ resources/views/layouts/user/header.blade.php
@@ -13,7 +13,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}"><img class="logo" src="{{ asset('logo.png') }}" alt=""></a>
+            <a class="navbar-brand" href="{{ url('/') }}"><img class="logo" src="{{ secure_asset('logo.png') }}" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
